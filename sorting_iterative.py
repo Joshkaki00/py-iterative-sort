ccae15791721d551255cc4ffc7bbc41ee4d15c4e
Joshkaki00 <josh.kakinuki@students.dominican.edu>
2026-02-27 22:38:19 -0800

    Enhance selection_sort documentation in sorting_iterative.py by specifying running time and memory usage, and implement the sorting logic with detailed comments.

--- sorting_iterative.py
+++ sorting_iterative.py
@@ -24,11 +24,18 @@ def bubble_sort(items):
 def selection_sort(items):
     """Sort given items by finding minimum item, swapping it with first
     unsorted item, and repeating until all items are in sorted order.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Repeat until all items are in sorted order
-    # TODO: Find minimum item in unsorted items
-    # TODO: Swap it with first unsorted item
+    Running time: O(n^2) - Two nested loops regardless of input
+    Memory usage: O(1) - Sorts in place with constant extra space"""
+    # Repeat until all items are in sorted order
+    for i in range(len(items) - 1):
+        # Find minimum item in unsorted items
+        min_index = i
+        for j in range(i + 1, len(items)):
+            if items[j] < items[min_index]:
+                min_index = j
+        # Swap it with first unsorted item
+        if min_index != i:
+            items[i], items[min_index] = items[min_index], items[i]
 
 
 def insertion_sort(items):
